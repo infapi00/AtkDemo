0a399c92e11d10812d6a24c99dbfc48444134345
Giuseppe <yogiuseppecapaldo93@gmail.com>
2019-03-26 11:32:01 +0100

    stil not working

--- AtkDemo.c
+++ AtkDemo.c
@@ -6,16 +6,61 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <atk/atk.h>
 #include <atk-bridge.h>
+
+/*
+static const gchar *
+get_toolkit_name (void)
+{
+  return "My Root";
+}
+
+static const gchar *
+get_toolkit_version (void)
+{
+  return atk_get_toolkit_version();
+}
+
+static AtkObject *
+get_root (void)
+{
+  static AtkObject *root;
+
+  if (!root)
+    {
+      root = g_object_new (ATK_TYPE_GOBJECT_ACCESSIBLE, NULL);
+      atk_object_initialize (root, NULL);
+    }
+
+  return root;
+}
+*/
+
+static void
+set_root (void){
+	static GObject *groot=NULL;
+	static AtkObject *root=NULL;
+
+	if(!groot){
+		groot=g_object_new (ATK_TYPE_GOBJECT_ACCESSIBLE, NULL);
+		root =atk_gobject_accessible_for_object(groot);
+		atk_object_initialize (root, NULL);
+	}
+}
+
 
 int main(int argc, char **argv) {
 
-	int bridge=atk_bridge_adaptor_init (&argc, &argv);
+	set_root;
+
+	int bridge = atk_bridge_adaptor_init (NULL, NULL);
+
 	if(bridge)
-		printf("Initialized\n",&bridge);
+		printf("Initialized\n");
 	else
-		printf("Not Initialized\n",&bridge);
-	//atk_bridge_adaptor_cleanup();
+		printf("Not Initialized\n");
+	atk_bridge_adaptor_cleanup();
 	return 0;
 
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
-CFLAGS = `pkg-config --cflags atk-bridge-2.0`
-LIBS = `pkg-config --libs atk-bridge-2.0`
+CFLAGS = `pkg-config --cflags glib-2.0 atk atk-bridge-2.0`
+LIBS = `pkg-config --libs glib-2.0 atk atk-bridge-2.0`
 
 all:	
 	gcc $(CFLAGS) -o AtkDemo.o AtkDemo.c $(LIBS)
