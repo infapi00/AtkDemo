d5ea9ba79500a4dac10d76c5046624ee73bdfba3
Giuseppe <giuseppecapaldo93@gmail.com>
2019-04-04 16:28:25 +0200

    update

--- fixAtk.h
+++ fixAtk.h
@@ -5,11 +5,9 @@
  *      Author: giuseppe
  */
 
-#ifndef FIXATK_H_
-#define FIXATK_H_
+#pragma once
 
 /* Work around the fact that Atk does not define the auto pointer cleanup.
  * Add a check using ATK_CHECK_VERSION once fixed in Atk upstream. */
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(AtkObject, g_object_unref)
 
-#endif /* FIXATK_H_ */
